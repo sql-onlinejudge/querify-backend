6c6b69094210a473f31ff67b15e3790064930a90
osoohynn <tbr335@naver.com>
2026-01-06 17:42:34 +0900

    feat: add test case entity

---
+++ src/main/kotlin/me/suhyun/soj/domain/problem/testcase/domain/TestCaseEntity.kt
@@ -0,0 +1,18 @@
+package me.suhyun.soj.domain.problem.testcase.domain
+
+import me.suhyun.soj.domain.problem.domain.ProblemEntity
+import org.jetbrains.exposed.dao.LongEntity
+import org.jetbrains.exposed.dao.LongEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+
+class TestCaseEntity(id: EntityID<Long>) : LongEntity(id) {
+    companion object : LongEntityClass<TestCaseEntity>(TestCaseTable)
+
+    var problem by ProblemEntity referencedOn TestCaseTable.problemId
+    var initSql by TestCaseTable.initSql
+    var answer by TestCaseTable.answer
+    var timeLimit by TestCaseTable.timeLimit
+    var createdAt by TestCaseTable.createdAt
+    var updatedAt by TestCaseTable.updatedAt
+    var deletedAt by TestCaseTable.deletedAt
+}
---
+++ src/main/kotlin/me/suhyun/soj/domain/problem/testcase/domain/TestCaseTable.kt
@@ -0,0 +1,11 @@
+package me.suhyun.soj.domain.problem.testcase.domain
+
+import me.suhyun.soj.domain.problem.domain.ProblemTable
+import me.suhyun.soj.global.entity.BaseTable
+
+object TestCaseTable : BaseTable("test_cases") {
+    val problemId = reference("problem_id", ProblemTable)
+    val initSql = text("init_sql")
+    val answer = text("answer")
+    val timeLimit = integer("time_limit")
+}
