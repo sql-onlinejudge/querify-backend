b2e118cf652ce09c81c2f286e56fcd1f96d43a87
osoohynn <tbr335@naver.com>
2026-03-12 14:42:29 +0900

    fix: create submit response 변경
    - String type. nullable의 name 컬럼 추가

--- src/main/kotlin/me/suhyun/soj/domain/submission/presentation/response/SubmitResponse.kt
+++ src/main/kotlin/me/suhyun/soj/domain/submission/presentation/response/SubmitResponse.kt
@@ -1,3 +1,3 @@
 package me.suhyun.soj.domain.submission.presentation.response
 
-data class SubmitResponse(val submissionId: Long, val score: Long? = null)
+data class SubmitResponse(val submissionId: Long, val name: String? = null)
