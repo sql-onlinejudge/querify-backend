995bdabd77378c2a8be83aeec9d5cd0e58774455
osoohynn <tbr335@naver.com>
2026-03-13 09:43:01 +0900

    fix: problem admin controller 경로 수정

--- src/main/kotlin/me/suhyun/soj/domain/admin/presentation/ProblemAdminController.kt
+++ src/main/kotlin/me/suhyun/soj/domain/admin/presentation/ProblemAdminController.kt
@@ -1,4 +1,4 @@
-package me.suhyun.soj.domain.problem.presentation.api
+package me.suhyun.soj.domain.admin.presentation
 
 import me.suhyun.soj.domain.problem.domain.repository.ProblemRepository
 import me.suhyun.soj.domain.problem.infrastructure.elasticsearch.ProblemDocument
